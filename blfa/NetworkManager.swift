lanebreach/ios_app

commit 5d26a8178613a3a7debcb16b15aad4ea8f8272c9

iOS: improve location collection algorithm

blfa/NetworkManager.swift
@@ -209,7 +209,7 @@ class NetworkManager {
         }
     }
     
-    func uploadReport(image: UIImage, filename: String, currentLocation: CLLocationCoordinate2D,
+    func uploadReport(image: UIImage, filename: String, location: CLLocationCoordinate2D,
                       emailAddress: String?, fullName: String?, phoneNumber: String?,
                       category: String, description: String,
                       progressMessage: @escaping (String) -> Void,
@@ -239,8 +239,8 @@ class NetworkManager {
             var parameters = [
                 "api_key": UserDefaults.standard.bool(forKey: kUserDefaultsUsingDevServerKey) ? Keys.apiKey : Keys.apiKeyProduction,
                 "service_code": "5a6b5ac2d0521c1134854b01",
-                "lat": String(currentLocation.latitude),
-                "long": String(currentLocation.longitude),
+                "lat": String(location.latitude),
+                "long": String(location.longitude),
                 "media_url": mediaUrl,
                 "description": fullDescription,
                 "attribute[Nature_of_request]": "Blocking_Bicycle_Lane"

blfa/NewReportViewController.swift
@@ -53,7 +53,7 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
     var locationManager = CLLocationManager()
     var hud: JGProgressHUD?
     var tipViews: [TipIdentifier:EasyTipView] = [:]
-    var selectingLibraryPhoto = false
+    var currentLocation: (CLLocationCoordinate2D, Date)?
     var uploadAttempts = 0
     
     //MARK:- Internal methods
@@ -108,7 +108,9 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
         }
     }
     
-    func updateLocation(showAlertOnError: Bool) {
+    func startUpdatingLocation(showAlertOnError: Bool) {
+        self.currentLocation = nil
+        
         if CLLocationManager.locationServicesEnabled() == true {
             if CLLocationManager.authorizationStatus() == .restricted || CLLocationManager.authorizationStatus() == .denied ||  CLLocationManager.authorizationStatus() == .notDetermined {
                 self.locationManager.requestWhenInUseAuthorization()
@@ -140,11 +142,6 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
     
     // reset for the next submission
     func resetReport() {
-        self.imageView.image = nil
-        self.viewModel.haveImage.value = .none
-        self.viewModel.category.value = NewReportViewModel.defaultCategory
-        self.viewModel.description.value = ""
-        
         self.changePhotoAction(sender: nil)
     }
     
@@ -167,6 +164,11 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
         self.viewModel.locationStatusSignal.observeValues { value in
             print("locationStatusSignal: \(value)")
             self.locationImageView.image = UIImage(named: value ? "ic_location_black" : "ic_location_bad_black")
+            if !value {
+                self.startUpdatingLocation(showAlertOnError: false)
+            } else {
+                self.locationManager.stopUpdatingLocation()
+            }
         }
         
         self.postReportButton.reactive.isEnabled <~ self.viewModel.okToSendSignal
@@ -270,6 +272,8 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
         
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+        self.locationManager.stopUpdatingLocation()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -284,16 +288,11 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
         self.viewModel.fullName.value = UserDefaults.standard.string(forKey: kUserDefaultsNameKey)
         self.viewModel.phoneNumber.value = UserDefaults.standard.string(forKey: kUserDefaultsPhoneKey)
         
-        if !selectingLibraryPhoto && self.viewModel.haveImage.value != .libraryPhoto {
-            // get a new location if we didn't return from selecting a library photo (and don't have a previously selected library photo)
-            self.viewModel.currentLocation.value = nil
-            
-            updateLocation(showAlertOnError: true)
+        if self.viewModel.imageLocation.value == nil {
+            // continuously get locations if we don't have an image or if we have a camera photo but still need a location
+            startUpdatingLocation(showAlertOnError: true)
         }
         
-        // for next time
-        selectingLibraryPhoto = false
-
         // add tips, if any
         var tipView: EasyTipView
         if tipViews[.newReportMain] == nil && AppDelegate.shouldShowTip(id: .newReportMain) {
@@ -368,9 +367,6 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
         // Call capturePhoto method by passing our photo settings and a
         // delegate implementing AVCapturePhotoCaptureDelegate
         capturePhotoOutput.capturePhoto(with: photoSettings, delegate: self)
-        
-        // and update our location since we may have moved...
-        updateLocation(showAlertOnError: false)
     }
     
     @IBAction func photoLibraryAction(sender: UIButton) {
@@ -383,11 +379,17 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
         vc.allowsEditing = false
         vc.delegate = self
         present(vc, animated: true)
-        
-        selectingLibraryPhoto = true
     }
     
     @IBAction func changePhotoAction(sender: UIButton?) {
+        // reset model
+        self.imageView.image = nil
+        self.viewModel.haveImage.value = .none
+        self.viewModel.imageLocation.value = nil
+        self.viewModel.category.value = NewReportViewModel.defaultCategory
+        self.viewModel.description.value = ""
+
+        // hide keyboard, if any
         self.categoryTextField.resignFirstResponder()
         self.descriptionTextField.resignFirstResponder()
         
@@ -406,7 +408,7 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
         self.categoryTextField.resignFirstResponder()
         self.descriptionTextField.resignFirstResponder()
 
-        if self.viewModel.currentLocation.value != nil {
+        if self.viewModel.imageLocation.value != nil {
             switch self.viewModel.haveImage.value {
             case .none:
                 AppDelegate.showSimpleAlertWithOK(vc: self, "Current location found. You may now SEND a new report to 311 as long as you have taken a picture and selected a category. Or you can select a photo from your library that was captured with its location.")
@@ -446,14 +448,14 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
             return
         }
         
-        // this func should not get called if we don't have a currentLocation
-        guard let currentLocation = self.viewModel.currentLocation.value else {
+        // this func should not get called if we don't have an imageLocation
+        guard let imageLocation = self.viewModel.imageLocation.value else {
             assert(false)
             return
         }
         
         // we already checked a library image earlier
-        if !locationIsInSanFrancisco(currentLocation) {
+        if !locationIsInSanFrancisco(imageLocation) {
             AppDelegate.showSimpleAlertWithOK(vc: self, "Sorry, you appear to be outside San Francisco. This app is only used to report bike lane violations within SF.")
             return
         }
@@ -471,7 +473,7 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
         uploadAttempts += 1
         NetworkManager.shared.uploadReport(image: image,
                                            filename: filename,
-                                           currentLocation: currentLocation,
+                                           location: imageLocation,
                                            emailAddress: self.viewModel.emailAddress.value,
                                            fullName: self.viewModel.fullName.value,
                                            phoneNumber: self.viewModel.phoneNumber.value,
@@ -540,7 +542,7 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
             }
             
             if addAndResetReport {
-                ReportManager.shared.addReport(location: currentLocation,
+                ReportManager.shared.addReport(location: imageLocation,
                                                description: self.viewModel.description.value,
                                                category: self.viewModel.category.value,
                                                serviceRequestId: serviceRequestId,
@@ -554,18 +556,17 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
     
     //MARK:- CLLocationManagerDelegate methods
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard self.viewModel.haveImage.value != .libraryPhoto else {
-            // a library photo was selected since we started to find a location - so we don't need it now
-            return
-        }
-        
         if locations.count > 0 {
             let location = locations[0]
             // TOOD - what's a reasonable requirement for accuracy?
             if location.horizontalAccuracy < 50 {
                 print("got location \(location.coordinate)")
-                self.viewModel.currentLocation.value = location.coordinate
-                self.locationManager.stopUpdatingLocation()
+                self.currentLocation = (location.coordinate, Date())
+
+                // if the user took a photo and we're just waiting for a location, store it now
+                if self.viewModel.haveImage.value == .cameraPhoto && self.viewModel.imageLocation.value == nil {
+                    self.viewModel.imageLocation.value = location.coordinate
+                }
             }
         }
     }
@@ -624,8 +625,23 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
             self.view.layoutIfNeeded()
         }
         
-        // tell the model that we have an image
+        // tell the model that we have an image and a location (maybe)
         self.viewModel.haveImage.value = .cameraPhoto
+        if let currentLocation = self.currentLocation {
+            let age = currentLocation.1.timeIntervalSinceNow
+            print("location age: \(age)")
+            if age > -30 {
+                self.viewModel.imageLocation.value = currentLocation.0
+            }
+        }
+
+        // if we don't have a location yet for this image, kick the location manager (in theory, this is not necessary)
+        if self.viewModel.imageLocation.value == nil {
+            print("kicking the location manager")
+            self.locationManager.stopUpdatingLocation()
+            self.startUpdatingLocation(showAlertOnError: false)
+        }
+        
         uploadAttempts = 0
     }
     
@@ -662,8 +678,6 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
             return
         }
         
-        self.viewModel.currentLocation.value = coordinate
-
         captureSession?.stopRunning()
 
         // update the image, hide the preview + photo buttons, slide up the control panel
@@ -677,6 +691,7 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
         
         // tell the model that we have an image
         self.viewModel.haveImage.value = .libraryPhoto
+        self.viewModel.imageLocation.value = coordinate
         uploadAttempts = 0
     }
     

blfa/NewReportViewModel.swift
@@ -27,7 +27,7 @@ class NewReportViewModel {
     let description: MutableProperty<String>
     let category: MutableProperty<String>
     let haveImage: MutableProperty<PendingImageType>
-    let currentLocation: MutableProperty<CLLocationCoordinate2D?>
+    let imageLocation: MutableProperty<CLLocationCoordinate2D?>
     
     var categorySignal: Signal<String, NoError>
     var descriptionSignal: Signal<String, NoError>
@@ -45,24 +45,24 @@ class NewReportViewModel {
         self.description = MutableProperty("")
         self.category = MutableProperty("")
         self.haveImage = MutableProperty(.none)
-        self.currentLocation = MutableProperty(nil)
+        self.imageLocation = MutableProperty(nil)
         
         self.categorySignal = self.category.signal
         self.descriptionSignal = self.description.signal
         
         // output true if the email address has 3+ chars and we have a valid category/image/location
-        self.okToSendSignal = Signal.combineLatest(self.category.signal, self.haveImage.signal, self.currentLocation.signal)
+        self.okToSendSignal = Signal.combineLatest(self.category.signal, self.haveImage.signal, self.imageLocation.signal)
             .map { (arg) -> Bool in
                 
-                let (category, haveImage, currentLocation) = arg
+                let (category, haveImage, imageLocation) = arg
                 
-                print("category=\(category), haveImage=\(haveImage), currentLocation=\(String(describing: currentLocation))")
-                return (category.count > 1) && (haveImage != .none) && (currentLocation != nil)
+                print("category=\(category), haveImage=\(haveImage), imageLocation=\(String(describing: imageLocation))")
+                return (category.count > 1) && (haveImage != .none) && (imageLocation != nil)
         }
         
-        self.locationStatusSignal = self.currentLocation.signal
-            .map { (currentLocation) -> Bool in
-                return currentLocation != nil
+        self.locationStatusSignal = self.imageLocation.signal
+            .map { (imageLocation) -> Bool in
+                return imageLocation != nil
         }
         
         DispatchQueue.main.async {

commit d2818c21929f699ea74b599d4281324e6167cfdf

iOS: fix enabling of the upload button by cleaning up the ViewModel initializtion (broken by 5d26a81); use "background tasks" to improve uploading if the user backgrounds the app in the middle of the action; treat HTTP 4xx errors as permanent fatal errors (since this is what happens if 311 detects a dupe POST even if we think that the first one failed).

blfa/NetworkManager.swift
@@ -16,7 +16,7 @@ public class NetworkManagerError: NSError {
         case networkBadHTTPStatus = -2
         case networkResponseMalformed = -3
         case networkJsonDecodeFailure = -4
-        case dataTaskNullData = -5
+        case dataTaskNullDataOrError = -5
         case imageUploadFailed = -6
         case missingServiceRequestId = -7
         case missingServiceRequestIdAndToken = -8
@@ -78,6 +78,8 @@ class NetworkManager {
 
     static let shared = NetworkManager()
     var debugLastHttpPost: String?
+    var uploadReportTaskId: UIBackgroundTaskIdentifier?
+    var uploadReportCompletion: ((_ serviceRequestId: String?, _ token: String?, _ error: NetworkManagerError?) -> Void)?
     
     func updateTabBarStyleForCurrentServer(vc: UIViewController) {
         vc.tabBarController?.tabBar.barTintColor = UserDefaults.standard.bool(forKey: kUserDefaultsUsingDevServerKey) ? UIColor.red : UIColor.lightGray
@@ -188,9 +190,9 @@ class NetworkManager {
                 DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                     switch self.mockUploadReportCount {
                     case 1:
-                        completion(nil, nil, MockNetworkManagerError(.dataTaskNullData, description: "error='mock error'"))
+                        completion(nil, nil, MockNetworkManagerError(.dataTaskNullDataOrError, description: "error='mock error'"))
                     case 2:
-                        completion(nil, nil, MockNetworkManagerError(.networkBadHTTPStatus, httpStatusCode: 999))
+                        completion(nil, nil, MockNetworkManagerError(.networkBadHTTPStatus, httpStatusCode: 400))
                     case 3:
                         completion(nil, nil, MockNetworkManagerError(.missingServiceRequestIdAndToken))
                     case 4:
@@ -209,6 +211,39 @@ class NetworkManager {
         }
     }
     
+    func beginUploadReportTask() {
+        let taskId = UIApplication.shared.beginBackgroundTask(expirationHandler: {
+            self.endUploadReportTask()
+        })
+        
+        if taskId != UIBackgroundTaskInvalid {
+            print("*** starting background task with ID \(taskId)")
+            self.uploadReportTaskId = taskId
+        } else {
+            print("*** failed to start background task")
+            self.uploadReportTaskId = nil
+        }
+    }
+    
+    func endUploadReportTask() {
+        if let uploadReportTaskId = uploadReportTaskId {
+            print("*** ending background task with ID \(uploadReportTaskId)")
+            UIApplication.shared.endBackgroundTask(uploadReportTaskId)
+            self.uploadReportTaskId = nil
+        }
+    }
+    
+    func reportCompletionAndEndUploadReportTask(_ serviceRequestId: String?, _ token: String?, _ error: NetworkManagerError?) {
+        if let uploadReportCompletion = uploadReportCompletion {
+            uploadReportCompletion(serviceRequestId, token, error)
+            self.uploadReportCompletion = nil
+        } else {
+            assertionFailure()
+        }
+        
+        self.endUploadReportTask()
+    }
+    
     func uploadReport(image: UIImage, filename: String, location: CLLocationCoordinate2D,
                       emailAddress: String?, fullName: String?, phoneNumber: String?,
                       category: String, description: String,
@@ -220,11 +255,13 @@ class NetworkManager {
             return
         }
         
+        uploadReportCompletion = completion
+        beginUploadReportTask()
         progressMessage("Uploading image")
         self.uploadImage(with: UIImagePNGRepresentation(image)!, filename: filename) { (error) in
             print("\(Date().timeIntervalSince1970) start metadata upload")
             if let error = error {
-                completion(nil, nil, NetworkManagerError(.imageUploadFailed, description: "error='\(error)'"))
+                self.reportCompletionAndEndUploadReportTask(nil, nil, NetworkManagerError(.imageUploadFailed, description: "error='\(error)'"))
                 return
             }
             
@@ -294,7 +331,7 @@ class NetworkManager {
             let task = URLSession.shared.dataTask(with: request) { data, response, error in
                 DispatchQueue.main.async {
                     guard let data = data, error == nil else {                                                 // check for fundamental networking error
-                        completion(nil, nil, NetworkManagerError(.dataTaskNullData, description: "error='\(String(describing: error))'"))
+                        self.reportCompletionAndEndUploadReportTask(nil, nil, NetworkManagerError(.dataTaskNullDataOrError, description: "error='\(String(describing: error))'"))
                         return
                     }
                     
@@ -302,7 +339,7 @@ class NetworkManager {
                     if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 201 {           // check for http errors
                         print("statusCode should be 201, but is \(httpStatus.statusCode)")
                         print("response = \(httpStatus)")
-                        completion(nil, nil, NetworkManagerError(.networkBadHTTPStatus, httpStatusCode: httpStatus.statusCode))
+                        self.reportCompletionAndEndUploadReportTask(nil, nil, NetworkManagerError(.networkBadHTTPStatus, httpStatusCode: httpStatus.statusCode))
                         return
                     }
                     
@@ -315,24 +352,24 @@ class NetworkManager {
                         if let dictionary = json as? [[String: Any]] {
                             if let serviceRequestId = dictionary[0]["service_request_id"] as? String {
                                 print("serviceRequestId: \(serviceRequestId)")
-                                completion(serviceRequestId, nil, nil)
+                                self.reportCompletionAndEndUploadReportTask(serviceRequestId, nil, nil)
                             } else if let token = dictionary[0]["token"] as? String {
                                 // need a delay to allow 311 to get a service request ID
                                 DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
                                     self.getServiceRequestId(from: token, completion: { (serviceRequestId, error) in
                                         // don't pass the error, if any, along cuz we've always got a token
-                                        completion(serviceRequestId, token, nil)
+                                        self.reportCompletionAndEndUploadReportTask(serviceRequestId, token, nil)
                                     })
                                 }
                                 return
                             } else {
-                                completion(nil, nil, NetworkManagerError(.missingServiceRequestIdAndToken))
+                                self.reportCompletionAndEndUploadReportTask(nil, nil, NetworkManagerError(.missingServiceRequestIdAndToken))
                             }
                         } else {
-                            completion(nil, nil, NetworkManagerError(.networkJsonDecodeFailure))
+                            self.reportCompletionAndEndUploadReportTask(nil, nil, NetworkManagerError(.networkJsonDecodeFailure))
                         }
                     } else {
-                        completion(nil, nil, NetworkManagerError(.networkResponseMalformed))
+                        self.reportCompletionAndEndUploadReportTask(nil, nil, NetworkManagerError(.networkResponseMalformed))
                     }
                     
                     print("\(Date().timeIntervalSince1970) done metadata upload")

blfa/NewReportViewController.swift
@@ -156,7 +156,7 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
         }
 
         // model
-        viewModel = NewReportViewModel(initialCategory: NewReportViewModel.defaultCategory)
+        viewModel = NewReportViewModel()
         
         // react to model changes
         self.categoryTextField.reactive.text <~ self.viewModel.categorySignal
@@ -384,10 +384,7 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
     @IBAction func changePhotoAction(sender: UIButton?) {
         // reset model
         self.imageView.image = nil
-        self.viewModel.haveImage.value = .none
-        self.viewModel.imageLocation.value = nil
-        self.viewModel.category.value = NewReportViewModel.defaultCategory
-        self.viewModel.description.value = ""
+        self.viewModel.reset()
 
         // hide keyboard, if any
         self.categoryTextField.resignFirstResponder()
@@ -489,23 +486,28 @@ class NewReportViewController: UIViewController, CLLocationManagerDelegate, UINa
                 // report to crashlytics as non-fatal error
                 Crashlytics.sharedInstance().recordError(error)
                 
-                var gotTransientError = false
+                var gotTransientError: Bool
                 switch error.kind {
-                case .imageUploadFailed:
-                    AppDelegate.showSimpleAlertWithOK(vc: self, "ERROR: uploading the image failed. Please try again.")
+                case .imageUploadFailed, .dataTaskNullDataOrError:
                     gotTransientError = true
-                case .dataTaskNullData, .networkBadHTTPStatus:
-                    if self.showDebugMessages {
-                        AppDelegate.showSimpleAlertWithOK(vc: self, "ERROR: uploading the report failed with transient error: \(error)")
-                    } else {
-                        AppDelegate.showSimpleAlertWithOK(vc: self, "ERROR: uploading the report failed (code \(error.code)). Please try again.")
-                    }
-                    gotTransientError = true
+                case .networkBadHTTPStatus:
+                    // note: we treat HTTP status codes 400-499 as permanent errors
+                    gotTransientError = (error.code < 400) || (error.code > 499)
                 default:
-                    break
+                    gotTransientError = false
                 }
                 
-                if !gotTransientError {
+                if gotTransientError {
+                    if error.kind == .imageUploadFailed {
+                        AppDelegate.showSimpleAlertWithOK(vc: self, "ERROR: uploading the image failed. Please try again.")
+                    } else {
+                        if self.showDebugMessages {
+                            AppDelegate.showSimpleAlertWithOK(vc: self, "ERROR: uploading the report failed with transient error: \(error)")
+                        } else {
+                            AppDelegate.showSimpleAlertWithOK(vc: self, "ERROR: uploading the report failed (code \(error.code)). Please try again.")
+                        }
+                    }
+                } else {
                     if self.showDebugMessages {
                         AppDelegate.showSimpleAlertWithOK(vc: self, "ERROR: uploading the report failed with permanent error: \(error)")
                     } else {

blfa/NewReportViewModel.swift
@@ -24,8 +24,9 @@ class NewReportViewModel {
     let emailAddress: MutableProperty<String?>
     let fullName: MutableProperty<String?>
     let phoneNumber: MutableProperty<String?>
-    let description: MutableProperty<String>
+    
     let category: MutableProperty<String>
+    let description: MutableProperty<String>
     let haveImage: MutableProperty<PendingImageType>
     let imageLocation: MutableProperty<CLLocationCoordinate2D?>
     
@@ -38,10 +39,11 @@ class NewReportViewModel {
                                 "Commuter Shuttle", "Uber", "Lyft", "Uber/Lyft",
                                 defaultCategory]    //  TODO/FUTURE - let user enter optional text to replace "other"?
     
-    init(initialCategory: String) {
+    init() {
         self.emailAddress = MutableProperty(nil)
         self.fullName = MutableProperty(nil)
         self.phoneNumber = MutableProperty(nil)
+        
         self.description = MutableProperty("")
         self.category = MutableProperty("")
         self.haveImage = MutableProperty(.none)
@@ -68,7 +70,15 @@ class NewReportViewModel {
         DispatchQueue.main.async {
             // set this after configuring categorySignal so that this gets tracked as a change
             // do this async so that binding code executing immediately after we are constructed will complete first
-            self.category.value = initialCategory
+            self.reset()
         }
+    }
+    
+    // note: this does not reset the user properties (email/name/phone)
+    func reset() {
+        self.haveImage.value = .none
+        self.imageLocation.value = nil
+        self.category.value = NewReportViewModel.defaultCategory
+        self.description.value = ""
     }
 }

blfa/ReportManager.swift
@@ -60,9 +60,10 @@ class ReportManager {
             report["token"] = token
         }
 
-        if let httpPost = httpPost {
-            report["httpPost"] = httpPost
-        }
+        // this is only needed for debugging:
+//        if let httpPost = httpPost {
+//            report["httpPost"] = httpPost
+//        }
 
         reports?.append(report)
         